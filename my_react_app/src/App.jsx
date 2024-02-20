807df785931199048f97cde6d2b8235805ccf774
amjad78678 <81846358+amjad78678@users.noreply.github.com>
2024-02-20 16:29:30 +0530

    props,propsTypes,defaultProps

--- my_react_app/src/App.jsx
+++ my_react_app/src/App.jsx
@@ -1,6 +1,7 @@
 
 import Card from "./Card";
 import Button from "./Button";
+import Student from "./Student";
 
 function App() {
   
@@ -9,6 +10,11 @@ function App() {
       
         <Card />
          <Button/>
+         <Student name="Amjadali" age='nalaayiram' isStudent={true}  />
+         <Student name="Rithas" age={30} isStudent={true}  />
+         <Student name="Arun" age={30} isStudent={false}  />
+         <Student name="Afsal" age={30} isStudent={false}  />
+         <Student name='akash'/>
       </>
     );
 
