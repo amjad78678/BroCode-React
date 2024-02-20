1e9259f75b2982bfc0404c6584cf437d6205f695
amjad78678 <81846358+amjad78678@users.noreply.github.com>
2024-02-20 15:26:52 +0530

    After card component

---
+++ my_react_app/src/App.jsx
@@ -0,0 +1,17 @@
+
+import Card from "./Card";
+import Button from "./Button";
+
+function App() {
+  
+    return (
+      <>
+      
+        <Card />
+         <Button/>
+      </>
+    );
+
+  }
+
+export default App
---
+++ my_react_app/src/Button.jsx
@@ -0,0 +1,10 @@
+function Button(){
+
+   return(
+<button className="btn">Click me</button>
+
+   )
+
+}
+
+export default Button;
